8f4a42bdc9acdabf0f33fde30152ce7fe45338d4
Garvit Gandhi <garvitgandhi0313@gmail.com>
2026-03-29 22:21:33 +0530

    hello

---
+++ 2. Basic Maths/1. Extraction_of_digits.py
@@ -0,0 +1,7 @@
+N=int(input())
+while N>0:
+    d=N%10
+    print(d)
+    N//=10
+
+
---
+++ 2. Basic Maths/2. Counting_of_digits.py
@@ -0,0 +1,12 @@
+N=int(input())
+count=0
+while N>0:
+    last_digit=N%10
+    count+=1
+    N//=10
+
+print(count)
+
+
+# TC = O(logN)
+# SC = O(1)
