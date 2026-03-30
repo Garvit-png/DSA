a09d40fa5a58b9d3efffd746f0e03d13f48ec8b5
Garvit Gandhi <garvitgandhi0313@gmail.com>
2026-03-30 22:18:53 +0530

    hello

---
+++ 2. Basic Maths/8. Equailateral Algorithm.py
@@ -0,0 +1,12 @@
+a,b=map(int,input().split())
+while (a>0 and b>0):
+    if (a>b):
+        a=a%b
+    else:
+        b=b%a
+
+if a==0:print(b)
+else:print(a)
+
+
+#TC is log phi, because a,b keeps fluctuating
